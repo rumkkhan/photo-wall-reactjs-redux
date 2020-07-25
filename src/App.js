c9e6d95be956d148ef8584b87745d4d7d4bb3164
Ramzan Khan <rumkkhan>
2020-07-25 19:50:50 +0530

    inital-stage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from 'uuid';
+
+function App() {
+  return (
+    <div className="App">
+       Hi
+    </div>
+  );
+}
+
+export default App;
